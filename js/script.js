91b2fa5413259f9436cce758481eabde378eed33
Mehraj Hosen <meherajhosen786@gmail.com>
2025-03-18 23:24:35 +0600

    check logic add

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const num123= document.querySelector("#numbers")
 const specialChar = document.querySelector("#specialChar")
 const increment = document.querySelector(".increment")
 const decrement = document.querySelector(".decrement")
+const randomPass = document.querySelector(".password")
 
 let inputValue = parseInt(range.value);
 
@@ -15,58 +16,82 @@ const lowerCase= "abcdefghijklmnopqrstuvwxyz"
 const number= "123456789"
 const char= "@#$%&*{}!<>"
 
+
+
+
 range.addEventListener("input", function() {
   const inputValue= parseInt(this.value)
   lengthNumber.innerHTML= inputValue;
   console.log(typeof lengthNumber.innerHTML);
+  generateRandomPassword(inputValue) 
   return inputValue
 });
 ABC.addEventListener("input", function() {
-  if(ABC.checked==true){
-    console.log("hello");
-  }else{
-    console.log("not");
-  }
+  generateRandomPassword(inputValue) 
+  
 });
 abc.addEventListener("input", function() {
-  if(abc.checked==true){
-    console.log("hello");
-  }else{
-    console.log("not");
-  }
+  generateRandomPassword(inputValue) 
 });
 num123.addEventListener("input", function() {
-  if(num123.checked==true){
-    console.log("hello");
-  }else{
-    console.log("not");
-  }
+  generateRandomPassword(inputValue) 
 });
 specialChar.addEventListener("input", function() {
-  if(specialChar.checked==true){
-    console.log("hello");
-  }else{
-    console.log("not");
-  }
+  generateRandomPassword(inputValue) 
 });
 
 increment.addEventListener("click", function() {
+  
   if (inputValue < 20) {
     inputValue += 1;
     range.value=inputValue
     lengthNumber.innerHTML = inputValue;
+    generateRandomPassword(inputValue) 
     console.log("increment");
   }
 });
 
 decrement.addEventListener("click", function() {
-  if (inputValue > 0) {
+  if (inputValue > 4) {
     inputValue -= 1;
     range.value=inputValue
     lengthNumber.innerHTML = inputValue;
+    generateRandomPassword(inputValue) 
     console.log("decrement");
   }
 });
 
 
 
+function generateRandomPassword(length) {
+  let allChars = '';
+  
+  if (ABC.checked) {
+    allChars += upperCase;
+  }
+  if (abc.checked) {
+    allChars += lowerCase;
+  }
+  if (num123.checked) {
+    allChars += number;
+  }
+  if (specialChar.checked) {
+    allChars += char;
+  }
+
+if (ABC.checked || abc.checked || num123.checked || specialChar.checked) {
+    let password = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomIndex];
+    
+  randomPass.innerHTML=password
+  }
+}else{
+  randomPass.innerHTML=("Check any character")
+}
+
+return password
+}
+
+generateRandomPassword(inputValue) 
